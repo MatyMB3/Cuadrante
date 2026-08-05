55369c6cf535d716b81e675544e984738c36c739
MatyMB3 <146846619+MatyMB3@users.noreply.github.com>
2026-08-05 17:29:04 -0300

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,6 +4,3 @@ const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(url, anonKey);
-
-export const DEFAULT_ORGANIZER_ID = process.env
-  .NEXT_PUBLIC_DEFAULT_ORGANIZER_ID as string;
